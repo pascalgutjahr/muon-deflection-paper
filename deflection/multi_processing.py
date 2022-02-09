f30a64d400a44a28ba3cb028aca11f177b57b9e3
pascalgutjahr <pascalgutjahr@tu-dortmund.de>
2022-02-09 16:26:45 +0100

    add ideas for multi script

---
+++ deflection/multi_processing.py
@@ -0,0 +1,3 @@
+------> multi processing kann auch im jupyter notebook verwendet werden
+--> kopiere multi_03_new und füge zusätzlich noch die funktion deflection per interaction ein um bei bedarf auch die ablenkung pro 
+interaktion zu ermitteln 
