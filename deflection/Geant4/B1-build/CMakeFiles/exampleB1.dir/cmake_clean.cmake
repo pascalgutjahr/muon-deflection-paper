d180aaf4da6a72e381a88581c1d1058a16e0f525
pascalgutjahr <pascal.gutjahr@tu-dortmund.de>
2022-10-07 09:43:13 +0200

    add geant4

---
+++ deflection/Geant4/B1-build/CMakeFiles/exampleB1.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exampleB1.dir/exampleB1.cc.o"
+  "CMakeFiles/exampleB1.dir/exampleB1.cc.o.d"
+  "CMakeFiles/exampleB1.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/exampleB1.dir/src/ActionInitialization.cc.o.d"
+  "CMakeFiles/exampleB1.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/exampleB1.dir/src/DetectorConstruction.cc.o.d"
+  "CMakeFiles/exampleB1.dir/src/EventAction.cc.o"
+  "CMakeFiles/exampleB1.dir/src/EventAction.cc.o.d"
+  "CMakeFiles/exampleB1.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/exampleB1.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "CMakeFiles/exampleB1.dir/src/RunAction.cc.o"
+  "CMakeFiles/exampleB1.dir/src/RunAction.cc.o.d"
+  "CMakeFiles/exampleB1.dir/src/SteppingAction.cc.o"
+  "CMakeFiles/exampleB1.dir/src/SteppingAction.cc.o.d"
+  "exampleB1"
+  "exampleB1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exampleB1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
